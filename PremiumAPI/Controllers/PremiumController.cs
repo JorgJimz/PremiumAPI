c078c31bc896956f32042e4972f194d6d87e6686
Jorge Jiménez <jrg.jmnz.prd@gmail.com>
2022-10-09 19:16:25 -0500

    Agregar archivos de proyecto.

---
+++ PremiumAPI/Controllers/PremiumController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Globalization;
+
+namespace PremiumAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PremiumController : ControllerBase
+    {
+
+        private readonly IWebHostEnvironment _environment;
+
+        public PremiumController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [HttpPost]
+        public IEnumerable<PremiumClass> Get([FromBody] PremiumParam model)
+        {
+            IEnumerable<PremiumModel> premiumClasses = LoadData();
+
+            var query = from p in premiumClasses
+                        where p.Plan.Contains(model.Plan) &&
+                              (p.State == model.State || p.State.Equals("*")) &&
+                              (p.MonthOfBirth.Equals(model.DateOfBirth?.ToString("MMMM", new CultureInfo("en-US")))
+                              || p.MonthOfBirth.Equals("*")) &&
+                              (p.AgeStart <= model.Age && model.Age <= p.AgeEnd)
+                        select new PremiumClass { Carrier = p.Carrier, Premium = p.Premium };
+
+            return query.ToList();
+        }
+
+        [NonAction]
+        public IEnumerable<PremiumModel> LoadData()
+        {
+            var fullpath = Path.Combine(_environment.ContentRootPath, "Data/data.json");
+            var jsonData = System.IO.File.ReadAllText(fullpath);
+            var premiums = JsonConvert.DeserializeObject<List<PremiumModel>>(jsonData);
+            return premiums;
+        }
+
+    }
+}
---
+++ PremiumAPI/PremiumClass.cs
@@ -0,0 +1,29 @@
+﻿namespace PremiumAPI
+{
+
+    public class PremiumParam
+    {
+        public DateTime? DateOfBirth { get; set; }
+        public string? State { get; set; }
+        public int Age { get; set; }
+        public string? Plan { get; set; }
+    }
+
+
+    public class PremiumClass
+    {
+        public string Carrier { get; set; }
+        public decimal Premium { get; set; }
+    }
+
+    public class PremiumModel
+    {
+        public string Carrier { get; set; }
+        public string Plan { get; set; }
+        public string State { get; set; }
+        public string MonthOfBirth { get; set; }
+        public int AgeStart { get; set; }
+        public int AgeEnd { get; set; }
+        public decimal Premium { get; set; }
+    }
+}
